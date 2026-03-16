d8c6716a06c2a9e649ff4958db6653d5dbad768d
markolilu <marklumbpt@gmail.com>
2026-03-16 20:06:20 +0000

    Added models dir with category, user and post models + fields to use in backend with postgres. Package.json now has all dependencies, apart from ones needed for postgres

---
+++ models/post.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes, Sequelize } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    //posted_by
+    postedBy: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    //created_on
+    createdOn: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    //post_Id
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+// Export Post model
+module.exports = Post;
