70dd9c21c18cd65af449f9cce403075f46225383
markolilu <marklumbpt@gmail.com>
2026-03-17 19:49:38 +0000

    Have added routes and router to app.jsx including the pages to be selected // pages are currently empty and will need filling with the relevant components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {useState} from 'react';
 
 //import components here
+import Navbar from './components/NavBar';
 
-// import session provider here
+import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/register';
+import Account from './pages/account';
+
 import { SessionProvider } from './contexts/SessionContext';
 
 
 const App = () => {
+    const [selectedPage, setSelectedPage] = useState('home');
+
     return (
-        <div>
+        <Router>
             <SessionProvider>
                 <Header />
+                <Navbar selectedPage={selectedPage} onSetPage={setSelectedPage} />
                 <Routes>
-                    {/* IMPORT <Route path='' element={<Name_Of_Component />} */}
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/account" element={<Account />} />
                 </Routes>
                 <Footer />
             </SessionProvider>
-        </div>
+        </Router>
     )
 };
 
