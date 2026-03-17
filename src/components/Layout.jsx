095f7c44e2386dd836f2ce4743d7d1e26af80a6a
lienekl <lienekl@yahoo.com>
2026-03-17 22:08:17 +0000

    Added Layout, env file, updated app.jsx. Added about page template.

---
+++ src/components/Layout.jsx
@@ -0,0 +1,59 @@
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const pages = [
+  { name: 'Home', key: 'home' },
+  { name: 'About', key: 'about' },
+  { name: 'Contact', key: 'contact' },
+  { name: 'Account', key: 'account' },
+  { name: 'Login', key: 'login' }
+]
+
+const Layout = ({ children, selectedPage, onSetPage }) => {
+
+  const renderPageLinks = () => {
+        
+    return pages.map(page => (
+       <li
+          key={page.key}
+          style={{
+            ...styles.sidebarLink,
+            ...(page.key === selectedPage ? styles.selected : {}),
+          }}
+          onClick={() => onSetPage(page.key)}
+        >
+          {page.name}
+        </li>
+    ));
+  }
+
+
+  return (
+    <div style={styles.container}>
+      {/* Navigation */}
+      <Header />
+      <NavBar />
+
+      <div style={styles.main}>
+        {/* Sidebar */}
+        <aside style={styles.sidebar}>
+          <ul>
+           {renderPageLinks()}
+          </ul>
+        </aside>
+
+        {/* Content Area */}
+        <section style={styles.content}>
+          {children}
+        </section>
+      </div>
+
+      {/* Footer */}
+      <Footer />
+     
+    </div>
+  );
+};
+
+export default Layout;
