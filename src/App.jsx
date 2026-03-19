a3547b107fbb3ae0045a449d17162e17bd88de49
markolilu <marklumbpt@gmail.com>
2026-03-19 19:28:00 +0000

    up to date for jason to check

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { SessionProvider } from './contexts/SessionContext';
 
 //import components here
