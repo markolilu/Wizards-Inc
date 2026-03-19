ca00a461882b043a3c6baad7802a83bf3654863a
Anton Anton <iamanton45@gmail.com>
2026-03-19 20:07:14 +0000

    Switch from Supabase to MySQL (Aiven), remove .env from tracking

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
